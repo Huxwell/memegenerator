ab94d8679638a5812719b4ca87ed7b2bdd201a05
huxwell <filip.drapejkowski@gmail.com>
2016-03-10 16:30:40 +0100

    glasses and multiple memes added

--- face.py
+++ face.py
@@ -13,6 +13,8 @@ from __future__ import print_function
 import numpy as np
 import cv2
 import time
+import sys
+print(sys.version)
 
 # local modules
 from video import create_capture
@@ -28,6 +30,9 @@ def detect(img, cascade):
     #print(rects)
     #wait = input("PRESS ENTER TO CONTINUE.")
     return rects
+
+def nothing(x):
+    pass
 
 def draw_rects(img, rects, color):
     for x1, y1, x2, y2 in rects:
@@ -58,14 +63,27 @@ if __name__ == '__main__':
     nested = cv2.CascadeClassifier(nested_fn)
 
     cam = create_capture(video_src, fallback='synth:bg=../data/lena.jpg:noise=0.05')
-    meme = cv2.imread("memes/spock.jpg")
+    meme = cv2.imread("memes/meme0.jpg")
     meme_rects = detect(meme, cascade)
+    track = 0
 
     #draw_rects(meme, meme_rects, (0, 255, 0))
     #cv2.imshow('meme', meme)
+
+
+    cv2.namedWindow('facedetect')
+    cv2.createTrackbar("meme",'facedetect',0,2,nothing)
+
     rects = []
     img=[]
     while True:
+        new_track = cv2.getTrackbarPos('meme','facedetect')
+        if(track != new_track):
+            print(new_track)
+            track = new_track
+            meme = cv2.imread("memes/meme"+str(track)+".jpg")
+            meme_rects = detect(meme, cascade)
+
         ret, img = cam.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray = cv2.equalizeHist(gray)
@@ -75,7 +93,7 @@ if __name__ == '__main__':
         if len(rects)==0:
             time.sleep(.01)
             continue
-        print(rects)
+        #print(rects)
 
         vis = img.copy()
         #draw_rects(vis, rects, (0, 255, 0))
@@ -85,12 +103,35 @@ if __name__ == '__main__':
                 roi = gray[y1:y2, x1:x2]
                 vis_roi = vis[y1:y2, x1:x2]
                 subrects = detect(roi.copy(), nested)
-                draw_rects(vis_roi, subrects, (255, 0, 0))
+                #print(len(subrects))
+                if len(subrects) == 2:
+                    #middle:
+                    #x = int((subrects[0][0] + subrects[1][2]) / 2)
+                    #y = int((subrects[0][1] + subrects[1][3]) / 2)
+                    #print(str(x) + "   " + str(y))
+                    #cv2.rectangle(vis_roi, (x,y), (x+20, y+20), (0, 255, 0), 2)
+                    y = int((subrects[0][1] + subrects[1][1]) / 2)
+
+                    '''s_img = cv2.imread("memes/small_glasses.png")
+                    x_offset=y_offset=0
+                    vis_roi[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img'''
+                    x_offset=int(vis_roi.shape[0]*0.1)
+                    y_offset=y
+                    s_img = cv2.imread("memes/small_glasses.png",-1)
+                    s_img = cv2.resize(s_img,(int(vis_roi.shape[1]*0.8),int(vis_roi.shape[1]*0.38*0.8)))
+
+                    print(s_img.shape)
+                    for c in range(0,3):
+                        vis_roi[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1], c] =\
+                        s_img[:,:,c] * (s_img[:,:,3]/255.0) +  vis_roi[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1], c] * (1.0 - s_img[:,:,3]/255.0)
+
+                #draw_rects(vis_roi, subrects, (255, 0, 0))
+
+
         dt = clock() - t
         vis = rect_to_rect(meme,vis,meme_rects[0],rects[0])
         draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
         cv2.imshow('facedetect', vis)
-
 
 
 
